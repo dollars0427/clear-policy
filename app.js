4a6bd771698b700675538377e22b8e1c312dc7bc
Sardo <sardo.ip@nxtomo.hk>
2016-03-21 12:07:14 +0800

    Add the sample of config file, add base part of app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var aws = require('aws-sdk');
+var log4js = require('log4js');
+var nconf = require('nconf');
+nconf.file('config', __dirname + 'config.json');
 
+var logger = log4js.getLogger('APP_LOG');
+var accessKey = nconf.get('token').access_key;
+var accessSecert = nconf.get('token').access_secert;
